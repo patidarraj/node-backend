8fa3efc1935f40345169a73c45a88c9e6275d5d1
Raj <rp@Rajs-MacBook-Air.local>
2024-07-11 12:41:40 +0530

    brand and comp added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const PORT = process.env.PORT ||8080;
 
 
-const arr = [
+const arrBrand = [
     {
         percentage:30,
         keyword:"Hair conditioner"
@@ -31,17 +31,41 @@ const arr = [
     }
 ]
 
+const arrCom = [
+    {
+        percentage:30,
+        keyword:"C Wax"
+    },
+    {
+        percentage:10,
+        keyword:"C oil"
+    },
+    {
+        percentage:10,
+        keyword:"C shampoo"
+    }
+]
+
 app.use(cors());
 
 app.get("/data",(req,res)=>{
-   
-
+   if(req.query.type === "brand"){
     res.status(200).json({
         status:"success",
         data:{
-            arr:arr
+            arr:arrBrand
         }
     })
+   }else{
+    res.status(200).json({
+        status:"success",
+        data:{
+            arr:arrCom
+        }
+    })
+   }
+
+    
 })
 
 app.listen(PORT,()=>{
